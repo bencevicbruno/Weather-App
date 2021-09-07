42a17eb3895632dfc141f61d31c2408056c163a2
Bruno Benčević <bencevic.bruno@gmail.com>
2021-09-07 14:26:59 +0200

    Test for the first commit of this branch

--- Weather App/UI/HomeVC/HomeView.swift
+++ Weather App/UI/HomeVC/HomeView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class HomeView: UIView {
     private lazy var backgroundImage = UIImageView()
+    private lazy var testLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,6 +26,13 @@ class HomeView: UIView {
         backgroundImage.image = UIImage(named: "background")
         backgroundImage.contentMode = .scaleAspectFill
         self.addSubview(backgroundImage)
+        
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        testLabel.text = "TEST"
+        testLabel.textAlignment = .center
+        testLabel.font = UIFont.boldSystemFont(ofSize: 50)
+        testLabel.textColor = .white
+        self.addSubview(testLabel)
     }
     
     private func setupConstraints() {
@@ -32,7 +40,10 @@ class HomeView: UIView {
             backgroundImage.topAnchor.constraint(equalTo: self.topAnchor),
             backgroundImage.rightAnchor.constraint(equalTo: self.rightAnchor),
             backgroundImage.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            backgroundImage.leftAnchor.constraint(equalTo: self.leftAnchor)
+            backgroundImage.leftAnchor.constraint(equalTo: self.leftAnchor),
+            
+            testLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            testLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
         ])
     }
 }
