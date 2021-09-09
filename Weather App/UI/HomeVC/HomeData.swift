6fa62527a0dcd928e8f600beb45ad05da5688d72
Bruno Benčević <bencevic.bruno@gmail.com>
2021-09-09 12:15:31 +0200

    Merged Home and Settings Screen

---
+++ Weather App/UI/HomeVC/HomeData.swift
@@ -0,0 +1,19 @@
+//
+//  HomeData.swift
+//  Weather App
+//
+//  Created by Bruno Benčević on 9/7/21.
+//
+
+import Foundation
+
+struct HomeData {
+    let condition: String
+    let currentTemperature: Int
+    let cityName: String
+    let minTemperature: Float
+    let maxTemperature: Float
+    let humidity: Float?
+    let pressure: Float?
+    let wind: Float?
+}
