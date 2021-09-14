34255392eaf41c026315ee6b434b62e688c6f945
Bruno Benčević <bencevic.bruno@gmail.com>
2021-09-14 17:01:54 +0200

    Removed unnecessary ViewController extension

--- Weather App/Common/Services/GeonamesService.swift
+++ Weather App/Common/Services/GeonamesService.swift
@@ -19,7 +19,7 @@ class GeonamesService {
     func getListOfCities(prefixedWith prefix: String, errorNotifier: ((String, String?) -> Void)? = nil) -> [String] {
         var cities = [String]()
         
-        let urlSafePrefix = prefix.lowercased().makeURLSafe()
+        let urlSafePrefix = prefix.lowercased().toURLSafe()
         
         if let url = URL(string: "http://api.geonames.org/searchJSON?name_startsWith=\(urlSafePrefix)&maxRows=10&username=bencevic_bruno") {
             do {
--- Weather App/Extensions/StringExtensions.swift
+++ Weather App/Extensions/StringExtensions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension String {
-    func makeURLSafe() -> String {
+    func toURLSafe() -> String {
         let replacements = [
             " ": "%20",
             "č": "c",
